79040d8d058518703e4820eae9b9eca53251c38e
RahulYavvari <rahulyavvari04@gmail.com>
2025-09-12 22:37:48 +0530

    Added author to config.yaml

--- gitlm/cli/gitlm.py
+++ gitlm/cli/gitlm.py
@@ -10,7 +10,7 @@ def print_version(ctx, param, value):
     if not value or ctx.resilient_parsing:
         return
     click.echo(f"GitLM, version {config.__VERSION__}")
-    click.echo("Author: Rahul Yavvari")
+    click.echo(f"Author: {config.__AUTHOR__}")
     ctx.exit()
 
 @click.group()
